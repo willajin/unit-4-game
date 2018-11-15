61147db70317c8baa392e53d084dc04f18e9e070
Willa Jin <jin.willa@gmail.com>
2018-11-14 19:50:47 -0800

    added crystal images; added basic CSS; added pseudo code to JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+// initialize game with random number and random values assigned to crystals
+// on click for each crystal, add value of crystal to total score
+// if total score matches random number, user wins
+// if total score is greater than random number, user loses
+// restart game
+
+});
